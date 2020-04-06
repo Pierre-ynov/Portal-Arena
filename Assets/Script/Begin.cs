a082728b5dfbb13f130ba5629813b68df4a75fb9
JR-2897 <jean.ragueneau.pro@gmail.com>
2020-04-06 17:52:35 +0200

    Ajout ecrans debut et fin

---
+++ Assets/Script/Begin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Begin : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Jeu");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
--- Assets/Script/Fireball.cs
+++ Assets/Script/Fireball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fireball : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class Fireball : MonoBehaviour
                 {
                     Destroy(collision.gameObject);
                     //Ajouter la scène de victoire du parent de la Fireball
+                    SceneManager.LoadScene("Fin");
                 }
         }
         //Détruit l'entité

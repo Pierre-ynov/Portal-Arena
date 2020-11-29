c9b9d3a7c84cea899228cfea31186a30588fb0c5
Pierre VILLIERS <pierre.villiers@ynov.com>
2020-11-29 17:05:57 +0100

    Correction de la synchro UIBack

--- Assets/Script/UI/UIBack.cs
+++ Assets/Script/UI/UIBack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,6 +17,29 @@ public class UIBack : MonoBehaviour
     public Text ArmorPlayer2;
     public Text RespawnPointPlayer2;
 
+    //Idee de correction
+    public GameObject Player1, Player2;
+
+    void Start()
+    {
+        Player1 = GameObject.Find("Player1(Clone)");
+        Player2 = GameObject.Find("Player2(Clone)");
+    }
+
+    void Update()
+    {
+        // Rafraichi l'UI des joueurs 
+        if (Player1 != null && Player2 != null)
+        {
+            Refresh(Player1.GetComponent<Player>(), Player2.GetComponent<Player>());
+        }
+    }
+
+    /// <summary>
+    /// Rafraichisement de l'UI lors d'une partie
+    /// </summary>
+    /// <param name="Player1"></param>
+    /// <param name="Player2"></param>
     public void Refresh(Player Player1, Player Player2)
     {
         LifePlayer1.text = "L I F E        :   " + Player1.health.load + "/" + Player1.health.capacity;

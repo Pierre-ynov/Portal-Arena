ce05170fa1f27f96979fbaccec39363f841e221c
Pierre VILLIERS <pierre.villiers@ynov.com>
2020-12-13 18:12:41 +0100

    Configuration Back de la scène règle renommé codex

---
+++ Assets/Script/Scenes/ReturnButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnButton : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Début");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}

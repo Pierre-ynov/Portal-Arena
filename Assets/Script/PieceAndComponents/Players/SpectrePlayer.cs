adbb4cff81004f7f04814d3a7c6a433a67b3ff57
Pierre VILLIERS <pierre.villiers@ynov.com>
2021-01-06 16:15:51 +0100

    PVI - Réorganisation des AttackBase & des AttackSpecial & des Player

---
+++ Assets/Script/PieceAndComponents/Players/SpectrePlayer.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Script.Capacites.BaseAttacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpectrePlayer : Player
+{
+    public GameObject attackBasePrefab;
+
+    void Start()
+    {
+        ConfigurationPlayer();
+        baseAttack = new Slot<Capacite>(new FireballAttack(attackBasePrefab, player));
+    }
+}

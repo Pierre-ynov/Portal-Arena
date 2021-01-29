24e91da784d3c45301272eb70e3b764b0a8c40c1
Pierre-ynov <57938549+Pierre-ynov@users.noreply.github.com>
2021-01-29 15:07:47 +0100

    PVI - Mise à jour de la classe Slot (#12)
    * Mise à jour de la classe Slot
    
    * Correction du slot
    
    Co-authored-by: Pierre VILLIERS <pierre.villiers@ynov.com>

--- Assets/Script/PieceAndComponents/Slot.cs
+++ Assets/Script/PieceAndComponents/Slot.cs
@@ -2,20 +2,56 @@
 
 public class Slot<T> : MonoBehaviour
 {
-    public T slot;
-    public bool isReady;
-    public float cooldown { get; private set; }
-    private bool isEmpty;
 
-    public Slot(T s)
+    /// <summary>
+    /// Contient la classe fournit lors de l'appel du constructeur Slot<T>()
+    /// </summary>
+    public T slot
     {
-        isReady=true;
-        slot = s;
-        cooldown = Time.deltaTime;
-        isEmpty = false;
+        get
+        {
+            return _slot;
+        }
+        set{
+            _slot = value;
+            cooldown = Time.deltaTime;
+            isEmpty = slot as Consumable != null && (slot as Consumable).counter <= 0;
+            isReady = !isEmpty;
+        }
+
     }
 
-    public void generateCoolDown()
+    private T _slot;
+
+    /// <summary>
+    /// Définit si le slot est prêt à être utilisé
+    /// </summary>
+    public bool isReady;
+
+    /// <summary>
+    /// Définit le cooldown du slot
+    /// </summary>
+    public float cooldown { get; private set; }
+
+    /// <summary>
+    /// Définit si le slot est vide
+    /// </summary>
+    public bool isEmpty { get; private set; }
+
+    /// <summary>
+    /// Constructeur
+    /// </summary>
+    /// <param name="s"></param>
+    public Slot(T s)
+    {
+        slot = s;
+    }
+
+
+    /// <summary>
+    /// Génère un temps d'attente
+    /// </summary>
+    public void GenerateCoolDown()
     {
         if(slot as Capacite != null)
         {

427d7c0733bd1bcfed2f2c82ba6414683f4a33b0
JR-2897 <jean.ragueneau.pro@gmail.com>
2021-02-03 16:25:50 +0100

    Ajout changement map

---
+++ Assets/Script/Events/EventScorchedEarth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EventScorchedEarth : EventModificationMap
+{
+
+    public Tilemap map;
+    public Tilemap mapDestroy;
+    private float timeCooldown = 50f;
+    private bool isFinish = false;
+
+    void Start()
+    {
+        map.gameObject.SetActive(true);
+        mapDestroy.gameObject.SetActive(false);
+    }
+
+    public IEnumerator CoolDown()
+    {
+        map.gameObject.SetActive(false);
+        mapDestroy.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime((timeCooldown));
+        isFinish = true;
+    }
+
+    void StartCoolDown()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            StartCoroutine(CoolDown());
+            if (isFinish == true)
+            {
+                map.gameObject.SetActive(true);
+                mapDestroy.gameObject.SetActive(false);
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoolDown();
+    }
+
+}

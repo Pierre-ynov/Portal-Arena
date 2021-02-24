7402aad473c887ca429aaf656961c036b0f69884
Pierre-ynov <57938549+Pierre-ynov@users.noreply.github.com>
2021-02-24 16:46:29 +0100

    PVI - Intégration des bruitages de Mort, Blessure, Réapparition et d'Attaque de feu. (#18)
    Co-authored-by: Pierre VILLIERS <pierre.villiers@ynov.com>

---
+++ Assets/Script/audio/SoundManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public AudioSource Audio;
+
+    public AudioClip Respawn;
+
+    public static SoundManagerScript soundInstance;
+
+    private void Awake()
+    {
+        if (soundInstance != null && soundInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        soundInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}

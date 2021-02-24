c35d522ad06745bff6274a7fdc8db274661a2019
JR-2897 <jean.ragueneau.pro@gmail.com>
2021-02-24 16:52:12 +0100

    Ajout script non finalisé dégât tilemap terre brule

---
+++ Assets/Script/Map/HurtScorchedEarth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtScorchedEarth : MonoBehaviour
+{
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            if (collision.gameObject.tag == "Player1")
+            {
+                Debug.Log("Test entre joueur 1");
+            }
+            else if (collision.gameObject.tag == "Player2")
+            {
+                Debug.Log("Test entre joueur 2");
+            }
+        }
+    }
+}

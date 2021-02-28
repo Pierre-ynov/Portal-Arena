751a114cb4bad6e315ec4ff9594ace7315291097
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2021-02-28 18:44:37 +0100

    Ajout dégat sur terrain brulés

--- Assets/Script/Map/HurtScorchedEarth.cs
+++ Assets/Script/Map/HurtScorchedEarth.cs
@@ -1,20 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HurtScorchedEarth : MonoBehaviour
 {
+
+    private int damage = (int)Damage.medium;
+    private bool isHurtPlayer1 = false;
+    private bool isHurtPlayer2 = false;
+
     private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        if (collision.gameObject.tag == "Player1" && !isHurtPlayer1)
         {
-            if (collision.gameObject.tag == "Player1")
+            HurtPlayer(collision.gameObject);
+            isHurtPlayer1 = true;
+        }
+        if (collision.gameObject.tag == "Player2" && !isHurtPlayer2)
+        {
+            HurtPlayer(collision.gameObject);
+            isHurtPlayer2 = true;
+        }
+    }
+
+    private void HurtPlayer(GameObject gameObject)
+    {
+        Player player = gameObject.GetComponent<Player>();
+
+        if (player.Hurt(damage))
+        {
+            if (!player.CanRevive())
             {
-                Debug.Log("Test entre joueur 1");
-            }
-            else if (collision.gameObject.tag == "Player2")
-            {
-                Debug.Log("Test entre joueur 2");
+                    Destroy(gameObject);
+                    SceneManager.LoadScene("Fin");
             }
         }
     }

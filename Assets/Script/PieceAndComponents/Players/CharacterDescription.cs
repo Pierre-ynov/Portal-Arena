04f19cce009302ed5240520b2c90169bd517f0a5
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2021-03-19 17:22:58 +0100

    Ajout scene selection perso (front + back)

---
+++ Assets/Script/PieceAndComponents/Players/CharacterDescription.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterDescription : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// Variable contenant le nom du personnage
+    /// </summary>
+    public string nameCharacter;
+    /// <summary>
+    /// Variable contenant la description du personnage
+    /// </summary>
+    public string description;
+    /// <summary>
+    /// Variable contenant le nom de l'attaque de base du personnage
+    /// </summary>
+    public string baseAttack;
+    /// <summary>
+    /// Variable contenant le nom de l'attaque spéciale du personnage
+    /// </summary>
+    public string specialAttack;
+    public Sprite iconeCharacter1;
+    public Sprite iconeCharacter2;
+    #endregion
+
+    public Sprite GetIconCharacter(string player)
+    {
+        if (player == "Player1")
+        {
+            return iconeCharacter1;
+        }
+        else
+        {
+            return iconeCharacter2;
+        }
+    }
+}

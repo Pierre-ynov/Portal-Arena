7557c2defac99656d9d2929e20d045c3473b74be
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2021-03-21 17:14:32 +0100

    Ajout script + gameobject non finalisé attaque base Cap Oil

---
+++ Assets/Script/Capacites/BaseAttacks/TentacleStrokeAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleStrokeAttack : Capacite
+{
+    // Contient les gameObject qui seront créer par son attaque
+    public GameObject rightAttack;
+    public GameObject leftAttack;
+    public GameObject upAttack;
+    public GameObject downAttack;
+
+    void Start()
+    {
+        imgAttack = Resources.Load<Sprite>("icon_firewaves"); //à modifier
+        timeCooldown = 3;
+        isReady = true;
+    }
+
+    /// <summary>
+    /// Il va donnée un coup de tentacule vers la direction du regard de la Pièce
+    /// </summary>
+    /// <param name="dirx"></param>
+    /// <param name="diry"></param>
+    public override void Action(int dirx, int diry)
+    {
+        Vector3 position = parent.transform.position;
+        GameObject attack = GetDirectionAttack(dirx, diry);
+        TentacleStroke tentacleStroke = Instantiate(attack, position, Quaternion.identity).GetComponent<TentacleStroke>();
+        tentacleStroke.parent = parent;
+        tentacleStroke.transform.position = new Vector3(position.x + dirx * 1f, position.y + diry * 1f, position.z);
+    }
+
+    /// <summary>
+    /// Permet de retourné le gameObject de l'attaque dans la bonne direction
+    /// </summary>
+    /// <param name="dirx"></param>
+    /// <param name="diry"></param>
+    /// <returns></returns>
+    private GameObject GetDirectionAttack(int dirx, int diry)
+    {
+        if (diry == 1)
+        {
+            return upAttack;
+        }
+        if (diry == -1)
+        {
+            return downAttack;
+        }
+        if (dirx == -1)
+        {
+            return leftAttack;
+        }
+        return rightAttack;
+    }
+
+}

c25c1a0c8bf79d089af843c2bb126f07fa072f6f
ClemencePalau <69987598+ClemencePalau@users.noreply.github.com>
2021-03-21 17:35:44 +0100

    CPA - Interactivité boutons de la scène de début (#29)
    * Implémenter le changement de position au respawn
    
    * Créer la méthode de spawn de pièces, réinitaliser la vie et armure du joueur lors du respawn
    
    * Ajouter la méthode de Respawn du joueur, modifier la génération aléatoire de pièces
    
    * Ajouter au joueur la capacité d'utiliser un consommable
    
    * Ajouter la partie UI de l'utilisation d'objet, modifier les points de respawn
    
    * Créer la méthode de changement de transparence d'images UI, commenter les fonctionnalités d'utilisation d'objet
    
    * Modifier commentaire méthode de changement de transparence
    
    * Supprimer fonctionnalité quitter côté joueur
    
    * Changer condition utilisation objet
    
    * Intégrer et ajuster nouveau background scène de début
    
    * Transférer les infos player vers scène victoire
    
    * Changer position leaderboard UI et supprimer commentaires
    
    * Modifications suite à la PR
    
    * Créer le SceneManager dans la scène de début
    
    * Ajouter le chargement de scènes en cliquant sur les instructions textuelles
    
    * Ajuster les positions des boutons
    
    * Modifier l'emplacement des boutons et changer leur texte

--- Assets/Script/Scenes/Begin.cs
+++ Assets/Script/Scenes/Begin.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Begin : MonoBehaviour
 {
+    public Button ButtonForBegin;
+    public Button ButtonForExit;
+    public Button ButtonForConfig;
+    public Button ButtonForCodex;
+
+    public EventTrigger trigger;
+
+    void Start()
+    {
+        ButtonForBegin.onClick.AddListener(() => LoadGameScene("Jeu"));
+        ButtonForExit.onClick.AddListener(() => Application.Quit());
+        ButtonForConfig.onClick.AddListener(() => LoadGameScene("Configuration"));
+        ButtonForCodex.onClick.AddListener(() => LoadGameScene("Codex"));
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,5 +44,10 @@ public class Begin : MonoBehaviour
         {
             Application.Quit();
         }
+    }
+
+    void LoadGameScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }

b973818560cc45ba5435e77aa3dce69e80ae34fd
ClemencePalau <clemencepalau@gmail.com>
2021-04-07 10:15:52 +0200

    Ajouter la partie Element du LaserBeam

---
+++ Assets/Script/Capacites/SpecialAttacks/LaserBeam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    private int damage;
+
+    public GameObject parent;
+
+    private bool hasTouchedEnemy;
+
+    void Start()
+    {
+        hasTouchedEnemy = false;
+        damage = (int)Damage.strong;
+        StartCoroutine(GenerateTimeAttack(1));
+    }
+
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        //Permet de savoir si le gameobject est bien le joueur
+        if ((collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") &&
+            collision.gameObject != parent && !hasTouchedEnemy)
+        {
+            Player enemy = collision.gameObject.GetComponent<Player>();
+            enemy.HurtPlayer(damage);
+            hasTouchedEnemy = true;
+        }
+    }
+
+    protected IEnumerator GenerateTimeAttack(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}

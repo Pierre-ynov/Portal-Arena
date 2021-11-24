61bc19da6973db61fe67607414954e725bfbab51
Pierre-ynov <57938549+Pierre-ynov@users.noreply.github.com>
2021-11-24 15:04:15 +0100

    PVI - Structuration des minions + création des scriptes pour RobBombe (#51)
    * Fin de la structuration des minions + création des scriptes pour robbombe minion
    
    * Desactivation mode demo
    
    Co-authored-by: Pierre VILLIERS <pierre.villiers@ynov.com>
    Co-authored-by: JR-2897 <34342750+JR-2897@users.noreply.github.com>

---
+++ Assets/Script/Capacites/MinionAttacks/Explosion.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Script.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Script.Capacites.MinionAttacks
+{
+    public class Explosion : MonoBehaviour
+    {
+        private int damage;
+
+        public GameObject parent;
+
+        void Start()
+        {
+
+            if (!GameConfiguration.isDemo)
+            {
+                damage = (int)Damage.veryStrong;
+            }
+            else
+            {
+                damage = GameConfiguration.damageDemoBaseAttack;
+            }
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //Permet de savoir si le gameobject est bien le joueur
+            if ((collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") && collision.gameObject != parent)
+            {
+                Player enemy = collision.gameObject.GetComponent<Player>();
+                enemy.HurtPlayer(damage);
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.tag == "Obstacle")
+                //Détruit l'entité
+                Destroy(gameObject);
+        }
+
+    }
+}

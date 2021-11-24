66baa7eea79f379ceb052bd05c0abe9c682fefdf
Pierre VILLIERS <pierre.villiers@ynov.com>
2021-11-24 14:54:59 +0100

    Fin de la structuration des minions + création des scriptes pour robbombe minion

---
+++ Assets/Script/PieceAndComponents/Minions/RobBombeMinion.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Script.Capacites.MinionAttacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Script.PieceAndComponents.Minions
+{
+    public class RobBombeMinion : Minion
+    {
+        public GameObject attackMinionPrefab;
+
+        void Start()
+        {
+            InitialiseMinionComponents();
+            InitializeMinionAttack<ExplosionAttack>(attackMinionPrefab);
+        }
+
+        protected override void executeIAAction()
+        {
+            
+        }
+    }
+}

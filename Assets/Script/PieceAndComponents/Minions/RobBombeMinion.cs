6ed98dde57d79f733085598b31872e313caadbd7
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2022-01-05 11:59:23 +0100

    Ajout prefab minion + IA

--- Assets/Script/PieceAndComponents/Minions/RobBombeMinion.cs
+++ Assets/Script/PieceAndComponents/Minions/RobBombeMinion.cs
@@ -5,17 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Script.PieceAndComponents.Minions
 {
     public class RobBombeMinion : Minion
     {
         public GameObject attackMinionPrefab;
+        public NavMeshAgent agent;
+        public GameObject targetPlayer;
 
         void Start()
         {
             InitialiseMinionComponents();
             InitializeMinionAttack<ExplosionAttack>(attackMinionPrefab);
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+            agent.speed = 5;
+        }
+
+        void Update()
+        {
+            var target = targetPlayer.transform.position;
+            agent.destination = target;
         }
 
         protected override void executeIAAction()

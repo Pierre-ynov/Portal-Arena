9ded0209620e3a9cf964d7e02a85a0cb10a28d4b
Pierre-ynov <57938549+Pierre-ynov@users.noreply.github.com>
2022-01-15 16:00:09 +0100

    PVI - Ajout gestion audio + Vérification du dossier de sauvegarde (#60)
    * PVI - Ajout de la gestion audio dans la scène configuration
    
    * Ajout d'une vérification sur la création du dossier pour les sauvegardes
    
    Co-authored-by: Pierre VILLIERS <pierre.villiers@ynov.com>

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,21 +1,25 @@
-﻿using UnityEngine;
+﻿using Assets.Script.Configuration;
+using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioClip[] playlist;
     public AudioSource audioSource;
     private int musicIndex = 0;
+    private GameConfiguration configuration;
 
     void Start()
     {
+        configuration = GameObject.FindWithTag("configuration").GetComponent<GameConfiguration>();
         audioSource.clip = playlist[0];
+        audioSource.volume = configuration.musicVolume;
         audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-      if(!audioSource.isPlaying)
+        if (!audioSource.isPlaying)
         {
             PlayNextSong();
         }
@@ -25,6 +29,7 @@ public class AudioManager : MonoBehaviour
     {
         musicIndex = (musicIndex + 1) % playlist.Length;
         audioSource.clip = playlist[musicIndex];
+        audioSource.volume = configuration.musicVolume;
         audioSource.Play();
     }
 }

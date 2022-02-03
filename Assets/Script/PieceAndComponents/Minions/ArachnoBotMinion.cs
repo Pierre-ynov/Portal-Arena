fd0a027740388bdd303b71cc87c86fed290ea955
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2022-02-03 10:19:12 +0100

    Finalisation IA des minions

--- Assets/Script/PieceAndComponents/Minions/ArachnoBotMinion.cs
+++ Assets/Script/PieceAndComponents/Minions/ArachnoBotMinion.cs
@@ -1,51 +1,54 @@
 ﻿using Assets.Script.Capacites.MinionAttacks;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace Assets.Script.PieceAndComponents.Minions
+public class ArachnoBotMinion : Minion
 {
-    public class ArachnoBotMinion : Minion
+    public GameObject attackMinionPrefab;
+    public NavMeshAgent agent;
+    private int numberPlayer;
+    public GameObject parent;
+
+
+    void Start()
     {
-        public GameObject attackMinionPrefab;
-        public NavMeshAgent agent;
-        private string numberPlayer;
-        public GameObject parent;
+        InitialiseMinionComponents();
+        InitializeMinionAttack<ExplosionMinionAttack>(attackMinionPrefab, gameObject);
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.speed = 2.5f;
+    }
 
+    void Update()
+    {
+        agent.destination = targetPlayer.transform.position;
+    }
 
-        void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
         {
-            InitialiseMinionComponents();
-            InitializeMinionAttack<ExplosionMinionAttack>(attackMinionPrefab, gameObject);
-            agent.updateRotation = false;
-            agent.updateUpAxis = false;
-            agent.speed = 2.5f;
-            numberPlayer = Random.Range(1, 2).ToString();
-            targetPlayer = GameObject.FindGameObjectWithTag("Player" + numberPlayer);
+            int dirx = (int)parent.transform.position.x;
+            int diry = (int)parent.transform.position.y;
+            minionAttack.Action(dirx, diry);
+            Destroy(gameObject);
         }
+    }
 
-        void Update()
+    protected override void executeIAAction()
+    {
+
+    }
+
+    public override void setNumberTargetPlayer(int numberPlayer)
+    {
+        if (numberPlayer % 2 == 0)
         {
-            agent.destination = targetPlayer.transform.position;
+            targetPlayer = GameObject.FindGameObjectWithTag("Player1");
         }
-
-        private void OnCollisionEnter2D(Collision2D collision)
+        else
         {
-            if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
-            {
-                int dirx = (int) parent.transform.position.x;
-                int diry = (int) parent.transform.position.y;
-                minionAttack.Action(dirx, diry);
-                Destroy(gameObject);
-            }
-        }
-
-        protected override void executeIAAction()
-        {
-
+            targetPlayer = GameObject.FindGameObjectWithTag("Player2");
         }
     }
 }

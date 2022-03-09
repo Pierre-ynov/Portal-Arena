b3a490b01c41759857bb7497fe3353f03b6fa15f
JR-2897 <34342750+JR-2897@users.noreply.github.com>
2022-03-09 09:51:43 +0100

    JRG - Button retour menu scene selection personnages (#75)
    * Ajout bouton retour menu dans scene selection personnages
    
    * Enlever suppression AudioManager dans LoadBeginScene

--- Assets/Script/Scenes/SelectionPlayer.cs
+++ Assets/Script/Scenes/SelectionPlayer.cs
@@ -14,20 +14,17 @@ public class SelectionPlayer : MonoBehaviour
         DontDestroyOnLoad(selectionPlayerManager);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
-
     public void LoadGameScene()
     {
         UISoundManagerScript.soundInstance.PlaySound();
         selectionPlayerManager.GetComponent<SelectionPlayerManager>().updatePlayerName();
         Destroy(GameObject.Find("AudioManager"));
         SceneManager.LoadScene("Jeu");
+    }
+
+    public void LoadBeginScene()
+    {
+        UISoundManagerScript.soundInstance.PlaySound();
+        SceneManager.LoadScene("Début");
     }
 }
